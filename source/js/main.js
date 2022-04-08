af3c6cdd21506becbd445d4efd990f4f6ce21bc9
dmkrupin <dev.dmkrupin@gmail.com>
2022-04-08 17:24:25 +0300

    initial

---
+++ source/js/main.js
@@ -0,0 +1,21 @@
+import {iosVhFix} from './utils/ios-vh-fix';
+import {initModals} from './modules/modals/init-modals';
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    initModals();
+  });
+});
